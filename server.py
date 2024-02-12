6dde47c54ad2c344f2ea41e14ceab45902859aa9
Mywor2 <daasanchezr@udistrital.edu.co>
2024-02-11 22:01:07 -0500

    Refactor server.py to use Fuzzy Logic for data processing

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
 from flask_socketio import SocketIO
 from flask_cors import CORS
 from gevent import monkey
+import Fuzzy_Logic.Fuzzy as FzLo
+import json
+
 monkey.patch_all()
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 socketio = SocketIO(app, cors_allowed_origins="*")
+fuzzy = FzLo.Fuzylogic()
 
 @socketio.on('connect')
 def connect():
@@ -15,11 +19,13 @@ def connect():
 @socketio.on('disconnect')
 def disconnect():
     print("disconnect")
+    
 @app.route('/api/send_data', methods=['POST'])
 def capture_all_post_requests():
     data = request.get_data(as_text=True)
     socketio.emit('post_req', data)
-    return "OK"
+    fuzzy.AsingValue(json.loads(data))
+    return str(fuzzy.Calculate())
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
