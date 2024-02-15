3807c87241d2e25390742b06b569eb26d7898e7f
Mywor2 <daasanchezr@udistrital.edu.co>
2024-02-15 11:22:35 -0500

    Add global variables and functions for tracking time

--- server.py
+++ server.py
@@ -22,10 +22,44 @@ def disconnect():
     
 @app.route('/api/send_data', methods=['POST'])
 def capture_all_post_requests():
+    global tiempo_inicio
+    global corriendo
     data = request.get_data(as_text=True)
     socketio.emit('post_req', data)
     fuzzy.AsingValue(json.loads(data))
+    if float (fuzzy.Calculate()) < 50 and obtener_tiempo_transcurrido() == 0:
+        desi = fuzzy.CalculateDesition(float(fuzzy.Calculate), obtener_tiempo_transcurrido())
+        if desi[0]:
+            tiempo_inicio = 0
+            corriendo = False
+            return "True"
+        elif desi[1]:
+            corriendo = True
+            return "False"
     return str(fuzzy.Calculate())
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
+
+
+import threading
+import time
+
+tiempo_inicio = 0
+corriendo = True
+
+def actualizar_tiempo_inicio():
+    global tiempo_inicio
+    while corriendo:
+        tiempo_inicio = time.time()
+        time.sleep(1) 
+
+def obtener_tiempo_transcurrido():
+    return time.time() - tiempo_inicio
+
+
+hilo = threading.Thread(target=actualizar_tiempo_inicio)
+
+hilo.start()
+
+
