8a2fbff60fbd126f8aefbc51a6ff4477af6be86d
Mywor2 <daasanchezr@udistrital.edu.co>
2024-02-16 22:21:32 -0500

    Fix GPS data decoding in GpsTest.py

--- test/GpsTest.py
+++ test/GpsTest.py
@@ -1,20 +1,19 @@
 # This file allows you to verify the operation of the GPS sensor
 
-from machine import *
+import machine
 from Gps_information import  *
 
 # tx and rx pin of the gps connected to the () pin of the esp32
 tx_pin = 16
 rx_pin = 17
 
-gps = machine.UART(2, tx=rx_pin, rx=tx_pin, baudrate=9600)
+gps = machine.UART(2, rx=tx_pin, baudrate=9600)
 
 while True:
 
     if gps.any():
-        nmea = gps.decode('UTF-8')
-        GpsInformation.nmeaSentenceOrganizer(time_zone= -5, nmea_sentence=nmea)
-        gps_info = GpsInformation.getGps_information()
+        nmea = gps.readline().decode('utf-8')
+        GpsInformation.nmeaSentenceOrganizer(time_zone=-5, nmea_sentence=nmea)
+        gps_info = GpsInformation.getGpsInformation()
         print(gps_info)
-
-    pass
+        
